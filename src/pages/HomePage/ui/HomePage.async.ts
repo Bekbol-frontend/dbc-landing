2ec14fafefd0aabedb4ab50d5fb271cb49459419
Bekbol <bekbol.bv@gmail.com>
2025-08-09 12:17:59 +0500

    change image style not found

--- src/pages/HomePage/ui/HomePage.async.ts
+++ src/pages/HomePage/ui/HomePage.async.ts
@@ -1,10 +1,3 @@
 import { lazy } from "react";
 
-// export const HomePageAsync = lazy(() => import("./HomePage"));
-export const HomePageAsync = lazy(
-  () =>
-    new Promise((resolve) =>
-      // @ts-ignore
-      setTimeout(() => resolve(import("./HomePage")), 3000)
-    )
-);
+export const HomePageAsync = lazy(() => import("./HomePage"));
